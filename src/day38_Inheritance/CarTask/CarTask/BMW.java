ed4e6c61fe646677f55be1cadd29ecabf903530c
Enver <simon.sayimu@gmail.com>
2022-08-02 18:37:44 -0400

    CarClassFirst Commit Practice

--- src/day38_Inheritance/CarTask/CarTask/BMW.java
+++ src/day38_Inheritance/CarTask/CarTask/BMW.java
@@ -6,6 +6,13 @@ public class BMW extends Car {
     public BMW(String model, int year, double price, String color, int miles) {
         super("Tesla", model, year, price, color, miles);
 
-
     }
+    public void breaksDown(){
+        System.out.println(brand+" "+model+" is breaking down");
+    }
+
+    public void racing(){
+        System.out.println(brand+" "+model+" is a racing car");
+    }
+
 }
---
+++ src/day38_Inheritance/CarTask/CarTask/Tesla.java
@@ -0,0 +1,21 @@
+package day38_Inheritance.CarTask.CarTask;
+
+public class Tesla extends Car{
+
+    public Tesla(String brand, String model, int year, double price, String color, int miles) {
+        super("Tesla", model, year, price, color, miles);
+    }
+
+
+    public void autoPilot(){
+        System.out.println(brand+" "+model+" in autopilot mode");
+    }
+
+
+    public void racing(){
+        System.out.println(brand+" "+model+" is racing");
+    }
+
+
+
+}
