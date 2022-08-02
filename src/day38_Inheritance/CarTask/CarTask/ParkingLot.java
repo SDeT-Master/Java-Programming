c53b342b1667f2f251562bd2e2d98d05c1268688
Enver <simon.sayimu@gmail.com>
2022-08-02 18:51:40 -0400

    CarClassFirst Commit Practice

---
+++ src/day38_Inheritance/CarTask/CarTask/ParkingLot.java
@@ -0,0 +1,18 @@
+package day38_Inheritance.CarTask.CarTask;
+
+public class ParkingLot {
+    public static void main(String[] args) {
+
+        Toyota toyota=new Toyota("Model",2020,20000,"Gray",123456);
+
+        Tesla tesla= new Tesla("Tesla S",2022,94100,"Red",0);
+
+        BMW bmw= new BMW("X5",2022,68000,"Black",12000);
+
+
+
+
+
+
+    }
+}
--- src/day38_Inheritance/CarTask/CarTask/Tesla.java
+++ src/day38_Inheritance/CarTask/CarTask/Tesla.java
@@ -2,7 +2,7 @@ package day38_Inheritance.CarTask.CarTask;
 
 public class Tesla extends Car{
 
-    public Tesla(String brand, String model, int year, double price, String color, int miles) {
+    public Tesla(String model, int year, double price, String color, int miles) {
         super("Tesla", model, year, price, color, miles);
     }
 
