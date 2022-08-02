c9c51e55637df858be285d6571763bafd89565b1
Enver <simon.sayimu@gmail.com>
2022-08-02 15:27:35 -0400

    CarClassFirst Commit Practice

---
+++ src/day38_Inheritance/CarTask/CarTask/BMW.java
@@ -0,0 +1,11 @@
+package day38_Inheritance.CarTask.CarTask;
+
+public class BMW extends Car {
+
+
+    public BMW(String model, int year, double price, String color, int miles) {
+        super("Tesla", model, year, price, color, miles);
+
+
+    }
+}
